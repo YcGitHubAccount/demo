0a316b2a05e343463839a1d64fbcb215057904f4
zhangyuchao <zyc32877@163.com>
2019-12-13 09:47:31 +0800

    I am new to java

---
+++ src/main/java/com/example/demo/po/Person.java
@@ -0,0 +1,14 @@
+package com.example.demo.po;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: ZYC
+ * @Date: 2019-12-13
+ **/
+@Data
+public class Person {
+    private String name;
+    private String age;
+}
---
+++ src/main/java/com/example/demo/test/DownTransformation.java
@@ -0,0 +1,38 @@
+package com.example.demo.test;
+
+import com.example.demo.po.Person;
+
+/**
+ * @Description:
+ * @Author: ZYC
+ * @Date: 2019-12-13
+ **/
+public class DownTransformation {
+
+    private void test(){
+        /*
+        正常引用对象
+         */
+        Object noramlObject = new Object();
+        Person normalPerson = new Person();
+        /*
+        向上转型
+         */
+        Object monkey = new Person();
+        //Person转型为object,只能调用object方法
+        monkey.toString();
+        //如果要调用自己的方法,需要向下转型
+        ((Person) monkey).getAge();
+        /*
+        向下转型
+        前提是Person是Object子类
+        如果不是会抛出异常
+         */
+        Person person = (Person)noramlObject;
+        person.getAge();
+        if (person instanceof Object){
+            System.out.println("person 是 Object 的子类");
+        }
+
+    }
+}
